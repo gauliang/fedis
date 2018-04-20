7ef924bd4e21b52c514b504532efd212de3e3ead
高国良 <gauth@sina.cn>
2018-04-20 09:20:53 +0800

    Add proxy .

--- README.md
+++ README.md
@@ -12,79 +12,83 @@ fedis 是一个基于 gulp/gulp-plugins 的前端开发集成解决方案。
 
 集成了常用任务，后期会增加更多功能：
 
-功能  |  描述
----- | ---- 
-tmaker		| 模板中间语言解释引擎
-doc         | 文档生成
-switch      | 项目管理
-publish		| 发布项目 
-test  		| 测试
-SSI         | Server Side Include
+| 功能  |  描述 |
+| ---- | ----  |
+| tmaker | 模板中间语言解释引擎 |
+| doc | 文档生成 |
+| switch | 项目管理 |
+| publish | 发布项目 |
+| test | 测试 |
+| SSI | Server Side Include |
+| proxy | 支持proxy代理，用以实现 ajax 的跨域请求 |
 
 ## 安装 fedis
 
-安装过程可能会持续一段时间，请耐心等候。
+方式一：
 
-安装方式一：
-``` bash
+```bash
 $ git clone https://github.com/gauliang/fedis.git
-
 $ cd fedis
-
 $ npm install
-
 $ gulp
 ```
 
-安装方式二：
-```
+方式二：
+
+```bash
 npm install fedis
 ```
 
 ## 如何使用
+
 * 进入 fedis/ 目录
 * 新建项目 `gulp --switch project-name`
 * 执行 gulp 命令启动 fedis
 * 进入 fedis/app/ 开始工作
 
-#### 配置
+### 配置
 
 Fedis 根目录中有一个名为 projectInfo 的文件，该文件存储了fedis的配置信息，说明如下：
 
-配置项  | 取值 | 说明 |
---- | --- | --- | 
-language | `cn`或`en` | Tmaker 预览时使用何种语言, `cn` 为中文, `en` 为英文 |
-minifyCss | 布尔值 | 是否对输出的 CSS 进行压缩 |
-minifyJs | 布尔值 | 是否对输出的 JS 进行压缩 |
-hashVersion | 布尔值 | 是否在输出的附件引用末端添加 hash |
+| 配置项  | 取值 | 说明 |
+| --- | --- | --- |
+| language | `cn`或`en` | Tmaker 预览时使用何种语言, `cn` 为中文, `en` 为英文  |
+| minifyCss | 布尔值 | 是否对输出的 CSS 进行压缩  |
+| minifyJs | 布尔值 | 是否对输出的 JS 进行压缩  |
+| hashVersion | 布尔值 | 是否在输出的附件引用末端添加 hash  |
+| proxyTarget.api | url | 要代理的目标路径 |
+| proxyTarget.enp | url | 要代理的目标路径 |
 
+### 提醒
 
-#### 提醒
 * 模板文件 (html) 中引用的预览附件请存放在 `./app/data/` 中
 * CSS 中引用的附件请存放在 `./app/scss/` 目录下即可
 
-#### 显示/存档/新建/切换 项目
+### 显示/存档/新建/切换 项目
+
 * `$gulp --show` 显示全部已存档和当前工作项目。
 * `$gulp --archive` 存档当前工作项目。
 * `$gulp --switch project-name` 切换至 `project-name` 项目，如果 `project-name` 项目不存在则会新建。
 
-#### 修改默认项目模板
+### 修改默认项目模板
+
 每次切换到一个新的不存在的项目时都会创建一个新的项目，新项目内容在 archive/_init 目录中，修改这些文件即可
 
-#### 发布项目
+### 发布项目
+
 `publish` 任务用以发布当前工作项目到 release 目录。该过程会把项目转换为线上状态。
 还可以通过 `--param` 来更新版本号，语义请参考 [semver](https://docs.npmjs.com/misc/semver)
-```
+
+```bash
 $gulp publish --<major|minor|patch>
 ```
 
-
-
 ## 常见问题
+
 由于网络原因安装过程中极有可能出现下列问题，在后面给出了解决方案。可以使用 `cnpm` 命令进行安装。
 
-
 ## 鸣谢
+
 fedis 用到了很多开源软件，没有这些开源项目就没有fedis，在此对相关开源团队表示由衷的感谢！
 
 ## LICENSE
--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,65 @@
 'use strict';
 
-var gulp        = require('gulp'),
+var gulp = require('gulp'),
 	browserSync = require('browser-sync').create(),
-	SSI 		= require('browsersync-ssi'),
-	minifyCss   = require('gulp-clean-css'),
-	minify   	= require('gulp-minify'),
-	plumber     = require('gulp-plumber'),
-	sass        = require('gulp-sass'),
-	Tmaker      = require('gulp-tmaker'),
-	rm      	= require('gulp-rm'),
-	yargs      	= require('yargs'),
-	wjson		= require('write-json'),
-	verAppend 	= require('gulp-version-number'),
-	fs			= require('fs'),
-	useref		= require('gulp-useref'),
-	gulpif 		= require('gulp-if'),
-	path		= require('path'),
-	projectInfo	= require('./projectInfo.json'),
-	gulpSSI		= require('gulp-html-ssi'),
+	SSI = require('browsersync-ssi'),
+	minifyCss = require('gulp-clean-css'),
+	minify = require('gulp-minify'),
+	plumber = require('gulp-plumber'),
+	sass = require('gulp-sass'),
+	Tmaker = require('gulp-tmaker'),
+	rm = require('gulp-rm'),
+	yargs = require('yargs'),
+	wjson = require('write-json'),
+	verAppend = require('gulp-version-number'),
+	fs = require('fs'),
+	useref = require('gulp-useref'),
+	gulpif = require('gulp-if'),
+	path = require('path'),
+	projectInfo = require('./projectInfo.json'),
+	sourcemaps = require('gulp-sourcemaps'),
+	gulpSSI = require('gulp-html-ssi'),
+	proxy = require('http-proxy-middleware'),
 	autoprefixer = require('gulp-autoprefixer');
- 
+
 var switchProjectName = '_init';
 
 var fedisPath = {
-	htmlSrc : 'app/'+projectInfo.projectName+'/**/*.html',
-	htmlDist: 'dist/'+projectInfo.projectName+'/',
-	sassSrc : 'app/'+projectInfo.projectName+'/scss/**/*.scss',
-	sassDist : 'dist/'+projectInfo.projectName+'/styles/',
-	jsSrc	: 'app/'+projectInfo.projectName+'/scripts/**/*.js',
-	jsDist	: 'dist/'+projectInfo.projectName+'/scripts/',
-	dataSrc : './app/'+projectInfo.projectName+'/data/*.*',
-	dataDist: './dist/'+projectInfo.projectName+'/data/',
-	assetSrc: './app/'+projectInfo.projectName+'/scss/*.+(jpg|png|gif)',
-	assetDist: './dist/'+projectInfo.projectName+'/styles/'
+	htmlSrc: 'app/' + projectInfo.projectName + '/**/*.html',
+	htmlDist: 'dist/' + projectInfo.projectName + '/',
+	sassSrc: 'app/' + projectInfo.projectName + '/scss/**/*.scss',
+	sassDist: 'dist/' + projectInfo.projectName + '/styles/',
+	jsSrc: 'app/' + projectInfo.projectName + '/scripts/**/*.js',
+	jsDist: 'dist/' + projectInfo.projectName + '/scripts/',
+	dataSrc: './app/' + projectInfo.projectName + '/data/*.*',
+	dataDist: './dist/' + projectInfo.projectName + '/data/',
+	assetSrc: './app/' + projectInfo.projectName + '/scss/*.+(jpg|png|gif)',
+	assetDist: './dist/' + projectInfo.projectName + '/styles/'
 }
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['Tmaker', 'preview-data', 'sass', 'js'], function() {
+gulp.task('serve', ['Tmaker', 'preview-data', 'sass', 'js'], function () {
 
 	browserSync.init({
 		server: {
-			baseDir:["./dist/"+projectInfo.projectName],
-			middleware:SSI({
-				baseDir:'./dist/'+projectInfo.projectName,
-				ext:'.html',
-				version:'2.10.0'
-			})
+			baseDir: ["./dist/" + projectInfo.projectName],
+			middleware: [
+				SSI({
+					baseDir: './dist/' + projectInfo.projectName,
+					ext: '.html',
+					version: '2.10.0'
+				}),
+				proxy('/api', {
+					target: projectInfo.proxyTarget.api,
+					pathRewrite: { '^/api': '/' },
+					changeOrigin: true
+				}),
+				proxy('/enp', {
+					target: projectInfo.proxyTarget.enp,
+					pathRewrite: { '^/enp': '/' },
+					changeOrigin: true
+				})
+			]
 		}
 	});
 
@@ -57,16 +71,16 @@ gulp.task('serve', ['Tmaker', 'preview-data', 'sass', 'js'], function() {
 	console.log('当前项目 :' + projectInfo.projectName);
 });
 
-gulp.task('Tmaker',function () {
+gulp.task('Tmaker', function () {
 	return gulp.src(fedisPath.htmlSrc)
 		.pipe(plumber())
-		.pipe(Tmaker({isPreview:true,lang:projectInfo.language}))
+		.pipe(Tmaker({ isPreview: true, lang: projectInfo.language }))
 		.pipe(gulp.dest(fedisPath.htmlDist))
 		.pipe(browserSync.stream());
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+gulp.task('sass', function () {
 	return gulp.src(fedisPath.sassSrc)
 		// .pipe(compass({
 		// 	project: path.join(__dirname, 'app/'+projectInfo.projectName+'/scss'),
@@ -75,18 +89,23 @@ gulp.task('sass', function() {
 		// }))
 		.pipe(plumber())
 		.pipe(sass.sync().on('error', sass.logError))
-		.pipe(sass({outputStyle:"compact"}))
+		.pipe(sass({ outputStyle: "compact" }))
 		.pipe(autoprefixer({
-            browsers: ['last 5 versions']
-        }))
+			browsers: ['last 5 versions']
+		}))
 		.pipe(gulp.dest(fedisPath.sassDist))
 		.pipe(browserSync.stream());
 });
 
 // javscript files operate
-gulp.task('js', function(){
+gulp.task('js', function () {
 	return gulp.src(fedisPath.jsSrc)
 		.pipe(plumber())
+
+		.pipe(sourcemaps.init())
+		.pipe(minify())
+		.pipe(sourcemaps.write('./'))
+
 		.pipe(gulp.dest(fedisPath.jsDist))
 		.pipe(browserSync.stream());
 });
@@ -102,130 +121,128 @@ gulp.task('asset', function () {
 });
 
 // publish
-function semverUpdate(key){
+function semverUpdate(key) {
 
 	var versionJson,
-		semver = {		
-			major:0,
-			minor:1,
-			patch:2
+		semver = {
+			major: 0,
+			minor: 1,
+			patch: 2
 		},
 		semverArr;
-		
-	if(fs.existsSync('./app/'+projectInfo.projectName+'/version.json')){
-		versionJson  = require('./app/'+projectInfo.projectName+'/version.json');		
+
+	if (fs.existsSync('./app/' + projectInfo.projectName + '/version.json')) {
+		versionJson = require('./app/' + projectInfo.projectName + '/version.json');
 	} else {
 		versionJson = {
 			"version": "1.0.0"
 		}
 	}
-		
-	semverArr = versionJson.version.split('.');	
+
+	semverArr = versionJson.version.split('.');
 	semverArr[semver[key]] = (parseInt(semverArr[semver[key]]) + 1).toString();
-	
+
 	semverArr[1] = semver[key] < 1 ? 0 : semverArr[1];
 	semverArr[2] = semver[key] < 2 ? 0 : semverArr[2];
-	
+
 	versionJson.version = semverArr.join('.');
-	wjson.sync('./app/'+projectInfo.projectName+'/version.json',versionJson);
+	wjson.sync('./app/' + projectInfo.projectName + '/version.json', versionJson);
 	return versionJson.version;
 }
 
-gulp.task('publish', function(){
-	if(projectInfo.projectName == 'null'){
+gulp.task('publish', function () {
+	if (projectInfo.projectName == 'null') {
 		console.log('\n 请先执行 "gulp --switch projectName" 新建一个项目\n');
 		return null;
 	}
 	var key = (yargs.argv.major && 'major') || (yargs.argv.minor && 'minor') || (yargs.argv.patch && 'patch') || 'patch';
 	var gulpStream = null;
-	
+
 
 	var version = semverUpdate(key);
 
 	gulp.src(fedisPath.assetSrc)
-		.pipe(gulp.dest('release/'+projectInfo.projectName + '-' + version));
-		
+		.pipe(gulp.dest('release/' + projectInfo.projectName + '-' + version));
+
 	gulpStream = gulp.src(fedisPath.htmlSrc)
 		.pipe(plumber())
-		.pipe(gulpSSI())		
-		.pipe(Tmaker({isPreview:false}))
-		.pipe(useref({ searchPath: fedisPath.htmlDist}));
-		
-		
-	if(projectInfo.minifyCss){
+		.pipe(gulpSSI())
+		.pipe(Tmaker({ isPreview: false }))
+		.pipe(useref({ searchPath: fedisPath.htmlDist }));
+
+	if (projectInfo.minifyCss) {
 		gulpStream = gulpStream.pipe(gulpif('*.css', minifyCss()));
 	}
 
-	if(projectInfo.minifyJs){
+	if (projectInfo.minifyJs) {
 		gulpStream = gulpStream.pipe(gulpif('*.js', minify()));
+
 	}
-	
-	if(projectInfo.versionHash){
-		gulpStream = gulpStream.pipe(verAppend({'append':{key:'v',to:[['css','%MD5%'],['js','%MD5%']]}}));
+
+	if (projectInfo.versionHash) {
+		gulpStream = gulpStream.pipe(verAppend({ 'append': { key: 'v', to: [['css', '%MD5%'], ['js', '%MD5%']] } }));
 	}
-		
-	gulpStream.pipe(gulp.dest('release/'+projectInfo.projectName + '-' + version))
-		.on('end',function(){
-			console.log('已发布项目 ' + projectInfo.projectName + '-' + version)}
+
+	gulpStream.pipe(gulp.dest('release/' + projectInfo.projectName + '-' + version))
+		.on('end', function () {
+			console.log('已发布项目 ' + projectInfo.projectName + '-' + version)
+		}
 		);
 });
 
 // switch project
-gulp.task('switch', function(cb){
-	
+gulp.task('switch', function (cb) {
+
 	// 切换/新建 项目
-	if(yargs.argv.switch == 'null'){
+	if (yargs.argv.switch == 'null') {
 		console.log('\n 项目名不能为 null');
-		return ;
+		return;
 	}
-	
-	if(yargs.argv.switch){
+
+	if (yargs.argv.switch) {
 		switchProjectName = yargs.argv.switch;
 
 		// 如果要切换的项目是 _init 或 已经在要切换的项目，直接返回。
-		if(switchProjectName == projectInfo.projectName || projectInfo.projectName == '_init'){
-			return ;
+		if (switchProjectName == projectInfo.projectName || projectInfo.projectName == '_init') {
+			return;
 		}
-		
-		if(fs.existsSync('app/'+ switchProjectName))
-		{
+
+		if (fs.existsSync('app/' + switchProjectName)) {
 			// 切换项目
 			projectInfo.projectName = switchProjectName;
-			wjson.sync('projectInfo.json',projectInfo);
+			wjson.sync('projectInfo.json', projectInfo);
 			console.log('已切换到项目 ' + switchProjectName);
 		}
-		else
-		{
+		else {
 			// 新建项目
-			console.log('新建 '+switchProjectName+' 项目');
-			gulp.src('app/_init/**/*',{ dot: true })
-				.pipe(gulp.dest('app/'+ switchProjectName))
-				.on('end',function () {
+			console.log('新建 ' + switchProjectName + ' 项目');
+			gulp.src('app/_init/**/*', { dot: true })
+				.pipe(gulp.dest('app/' + switchProjectName))
+				.on('end', function () {
 					projectInfo.projectName = switchProjectName;
-					wjson.sync('projectInfo.json',projectInfo);
+					wjson.sync('projectInfo.json', projectInfo);
 					process.stdout.write(`新建项目完成\n`);
 				});
 		}
 		return;
 	}
-	
-	if(projectInfo.projectName == 'null'){
+
+	if (projectInfo.projectName == 'null') {
 		console.log('\n 请先执行 "gulp --switch projectName" 新建一个项目');
-		return ;
+		return;
 	}
-	
+
 	// 显示当前项目列表
-	if(yargs.argv.show)
-	{
+	if (yargs.argv.show) {
 		console.log('\n 当前项目 :\n');
-		fs.readdirSync('app/').forEach(function(item) {
-			if(item == '_init') {return;}			
+		fs.readdirSync('app/').forEach(function (item) {
+			if (item == '_init') { return; }
 			console.log('   ' + item);
 		})
 		return console.log('\n 已列出所有项目\n');
 	}
-	
+
 	gulp.start('serve');
 });
 
-gulp.task('default',['switch']);
+gulp.task('default', ['switch']);
